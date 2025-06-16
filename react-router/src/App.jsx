76e995158b55af9c3e21073115c7acd9e08ba0a8
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-16 10:48:05 +0000

    react router

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
