96dba17eb619132f8886a0eddc5cfaeccd455952
MiquelMarinoEspinosa <miquel.marino.espinosa@gmail.com>
2025-06-17 08:39:45 +0000

    nesting routes

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -3,6 +3,8 @@ import Homepage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
 import PageNotFound from "./pages/PageNotFound";
 import Nav from "./pages/Nav";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
@@ -10,7 +12,10 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- react-router/src/pages/Dashboard.jsx
+++ react-router/src/pages/Dashboard.jsx
@@ -1,3 +1,18 @@
+import { Link, Outlet } from "react-router-dom";
+
 export default function Dashboard() {
-  return <div>This is a dashboard</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="settings">Settings</Link>
+        </li>
+      </ul>
+      This is a Dashboard
+      <Outlet />
+    </div>
+  );
 }
